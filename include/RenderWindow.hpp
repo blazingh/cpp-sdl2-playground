4e4cd279a6a17f58fa7a5a6f0cf23869f4e82480
blazingh <hadibaalbaki2@gmail.com>
2023-08-20 00:28:34 +0300

    added new renderwindow methods

--- include/RenderWindow.hpp
+++ include/RenderWindow.hpp
@@ -7,6 +7,9 @@ class RenderWindow
 public:
   RenderWindow(const char* title, int width, int height);
   void loadTexture(const char* filename);
+  void clear();
+  void render(SDL_Texture* texture);
+  void display();
   void cleanUp();
 private:
   SDL_Window* window;
--- src/renderwindow.cpp
+++ src/renderwindow.cpp
@@ -25,6 +25,36 @@ RenderWindow::RenderWindow(const char* title, int width, int height)
 
 }
 
+SDL_Texture* RenderWindow::loadTexture(const char* filename)
+{
+
+    SDL_Texture* texture = NULL
+    texture = IMG_LoadTexture(renderer, filename);
+
+    if (texture == NULL)
+    {
+        std::cout << "Failed to load texture: " << SDL_GetError() << std::endl;
+        return NULL;
+    }
+
+    return texture;
+}
+
+void RenderWindow::clear()
+{
+    SDL_RenderClear(renderer);
+}
+
+void RenderWindow::render(SDL_Texture* texture)
+{
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+}
+
+void RenderWindow::display()
+{
+    SDL_RenderPresent(renderer);
+}
+
 void RenderWindow::cleanUp()
 {
     SDL_DestroyWindow(window);
